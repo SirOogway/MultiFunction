c3e709907f5e19e388664af75f58cf4a83a6e0a8
ChonchitoFeliz <jesusandresrodriguezgarcia@outlook.com>
2022-08-19 22:49:02 -0500

    Added menu, to do -> add interface and functionalities

--- app/src/main/java/com/innova/multifunction/MainActivity.kt
+++ app/src/main/java/com/innova/multifunction/MainActivity.kt
@@ -2,10 +2,28 @@ package com.innova.multifunction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.is_prime_number_action -> Toast.makeText(this, "hola", Toast.LENGTH_SHORT).show()
+            R.id.is_perfect_number_action -> TODO()
+            R.id.is_palindrome_number_action -> TODO()
+            else -> Toast.makeText(this, "chao", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
